56d2d64a9ddebcb535053e5b25eaab4aaf964716
xynydev <eino.rauhala@proton.me>
2024-01-31 17:25:12 +0200

    chore: scaffolding for missing documentation pages to help contributors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,69 +1,107 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blue-build.org/',
+  site: "https://blue-build.org/",
   integrations: [
     starlight({
-      title: 'BlueBuild',
+      title: "BlueBuild",
       logo: {
         replacesTitle: true,
-        dark: './src/assets/logo-dark.svg',
-        light: './src/assets/logo-light.svg',
-        alt: 'BlueBuild. A minimal logo with a blue-billed duck holding a golden wrench in its beak.',
+        dark: "./src/assets/logo-dark.svg",
+        light: "./src/assets/logo-light.svg",
+        alt: "BlueBuild. A minimal logo with a blue-billed duck holding a golden wrench in its beak.",
       },
       editLink: {
-        baseUrl: 'https://github.com/blue-build/website/edit/main/'
+        baseUrl: "https://github.com/blue-build/website/edit/main/",
       },
       social: {
-        github: 'https://github.com/blue-build/'
+        github: "https://github.com/blue-build/",
       },
-      sidebar: [{
-        label: 'Learn',
-        items: [{
-          label: 'Getting started',
-          link: '/learn/getting-started/'
-        }, {
-          label: 'Scope',
-          link: '/learn/scope/'
-        }]
-      }, {
-        label: 'How-to',
-        autogenerate: {
-          directory: 'how-to'
-        }
-      }, {
-        label: 'Reference',
-        items: [{
-          label: 'recipe.yml',
-          link: '/reference/recipe/'
-        }, {
-          label: 'Modules',
+      sidebar: [
+        {
+          label: "Learn",
+          items: [
+            {
+              label: "Getting started",
+              link: "/learn/getting-started/",
+            },
+            {
+              label: "Thinking like a distribution",
+              link: "/learn/mindset/",
+            },
+            {
+              label: "Building on Universal Blue",
+              link: "/learn/universal-blue/",
+            },
+            {
+              label: "How BlueBuild works",
+              link: "/learn/how/",
+            },
+            {
+              label: "Contributing",
+              link: "/learn/contributing/",
+            },
+            {
+              label: "Scope",
+              link: "/learn/scope/",
+            },
+          ],
+        },
+        {
+          label: "How-to",
           autogenerate: {
-            directory: 'reference/modules'
-          }
-        }]
-      }],
-      customCss: ['@fontsource/atkinson-hyperlegible/400.css', '@fontsource/atkinson-hyperlegible/700.css', '@fontsource-variable/rubik', '@fontsource/ibm-plex-mono', './src/tailwind.css'],
+            directory: "how-to",
+          },
+        },
+        {
+          label: "Reference",
+          items: [
+            {
+              label: "recipe.yml",
+              link: "/reference/recipe/",
+            },
+            {
+              label: "module",
+              link: "/reference/module/",
+            },
+            {
+              label: "Modules",
+              autogenerate: {
+                directory: "reference/modules",
+              },
+            },
+          ],
+        },
+      ],
+      customCss: [
+        "@fontsource/atkinson-hyperlegible/400.css",
+        "@fontsource/atkinson-hyperlegible/700.css",
+        "@fontsource-variable/rubik",
+        "@fontsource/ibm-plex-mono",
+        "./src/tailwind.css",
+      ],
       components: {
-        SocialIcons: './src/components/NavLinks.astro',
-        Hero: './src/components/Hero.astro',
+        SocialIcons: "./src/components/NavLinks.astro",
+        Hero: "./src/components/Hero.astro",
       },
-      head: [{
-        tag: 'script',
-        attrs: {
-          defer: true,
-          src: 'https://eu.umami.is/script.js',
-          'data-website-id': 'fdbab42b-cab4-4f46-b06a-172700ea1e1c'
-        }
-      }]
+      head: [
+        {
+          tag: "script",
+          attrs: {
+            defer: true,
+            src: "https://eu.umami.is/script.js",
+            "data-website-id": "fdbab42b-cab4-4f46-b06a-172700ea1e1c",
+          },
+        },
+      ],
     }),
     tailwind({
-      applyBaseStyles: false
+      applyBaseStyles: false,
     }),
-    icon()
-  ]
+    icon(),
+  ],
 });
