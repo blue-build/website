cc7aa083e6b8aae2d6273fc6c72ca9398c447853
xynydev <60004820+xynydev@users.noreply.github.com>
2024-04-27 14:52:02 +0300

    feat: add blog rss feed

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,22 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
-    docs: defineCollection({ schema: docsSchema() }),
+    docs: defineCollection({
+        schema: docsSchema({
+            extend: z.object({
+                date: z.date().optional(),
+                authors: z
+                    .array(
+                        z.object({
+                            name: z.string(),
+                            img: z.string(),
+                            github: z.string(),
+                        }),
+                    )
+                    .optional(),
+            }),
+        }),
+    }),
     i18n: defineCollection({ type: "data", schema: i18nSchema() }),
 };
