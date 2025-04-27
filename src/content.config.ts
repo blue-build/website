c805d283d69dddeb2ac97bc1d5f0d6f0618e2947
xyny <git@xyny.anonaddy.me>
2025-04-27 20:19:20 +0300

    fix: use content collections properly, ts, etc.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,24 +1,24 @@
 import { defineCollection, z } from "astro:content";
 import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
-import { glob } from 'astro/loaders';
+import { glob } from "astro/loaders";
 
 export const collections = {
-    docs: defineCollection({
-        loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/docs" }),
-        schema: docsSchema({
-            extend: z.object({
-                date: z.date().optional(),
-                authors: z
-                    .array(
-                        z.object({
-                            name: z.string(),
-                            img: z.string(),
-                            github: z.string(),
-                        }),
-                    )
-                    .optional(),
+  docs: defineCollection({
+    loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/docs" }),
+    schema: docsSchema({
+      extend: z.object({
+        date: z.date().optional(),
+        authors: z
+          .array(
+            z.object({
+              name: z.string(),
+              img: z.string(),
+              github: z.string(),
             }),
-        }),
+          )
+          .optional(),
+      }),
     }),
-    i18n: defineCollection({ type: "data", schema: i18nSchema() }),
+  }),
+  i18n: defineCollection({ type: "data", schema: i18nSchema() }),
 };
